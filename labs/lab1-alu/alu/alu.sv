d8be2eee8d828a4702339a93f069c4fdbf41e22a
eleqc@icloud.com <102486771+ElenaQuintana04@users.noreply.github.com>
2026-09-02 14:45:50 +1000

    Refactor ALU module and implement zero flag logic
    Refactor ALU module to improve functionality and readability.

--- labs/lab1-alu/alu/alu.sv
+++ labs/lab1-alu/alu/alu.sv
@@ -5,21 +5,35 @@
 `default_nettype none
 
 //
-module alu(
-    input [4:0] alu_function,
-    input [31:0] op_a,
-    input [31:0] op_b,
-    output logic [31:0] result,
-    output result_eq_zero);
-
+module alu( input [4:0] alu_function, input [31:0] op_a, input [31:0] op_b, 
+            output logic [31:0] result, output logic result_eq_zero);
+    
     // Implement the zero flag
-    assign result_eq_zero = 1'b0;
+    assign result_eq_zero = (result == `ZERO);
 
     // Compute result
     always_comb begin
         case (alu_function)
             `ALU_ADD:   result = op_a + op_b;
             `ALU_SUB:   result = op_a - op_b;
+            `ALU_XOR:   result = op_a ^ op_b;
+            `ALU_OR :   result = op_a | op_b;
+            `ALU_AND:   result = op_a & op_b;
+            `ALU_SLL:   result = op_a << op_b[4:0];
+            `ALU_SRL:   result = op_a >> op_b[4:0];
+            `ALU_SRA:   result = $signed(op_a) >>> op_b[4:0];
+            `ALU_SLT:   begin
+                            if ($signed(op_a) < $signed(op_b))
+                                result = 1;
+                            else 
+                                result = `ZERO;
+                        end
+            `ALU_SLTU:  begin
+                            if (op_a < op_b)
+                                result = 1;
+                            else 
+                                result = `ZERO;
+                        end
             default: result = `ZERO;
         endcase
     end
